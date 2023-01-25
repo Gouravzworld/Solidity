e338b027608a7036c69f007f725d967c686c162d
Gourav <101053655+Gouravzworld@users.noreply.github.com>
2023-01-25 23:46:48 +0530

    Constructor

---
+++ constructor.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.5.0;	
+		
+
+contract constructorExample {	
+		
+	 
+	string str;	
+		
+	constructor() public {				
+		str = "GeeksForGeeks";	
+	}	
+
+	function getValue(
+	) public view returns (
+	string memory) {	
+		return str;	
+	}	
+}
