e180d079282476fab9ec68129c9daacacdb8e2ea
Gourav <101053655+Gouravzworld@users.noreply.github.com>
2023-01-29 23:54:16 +0530

    Add files via upload

---
+++ addition.sol
@@ -0,0 +1,23 @@
+/ SPDX-License-Identifier: GPL-3.0
+pragma solidity >= 0.4.16 < 0.7.0; 
+    
+ 
+contract Test
+{    
+    uint public var1;
+    uint public var2;
+    uint public sum;
+    
+     
+    function set(uint x, uint y) public
+    { 
+        var1 = x;
+        var2=y;
+        sum=var1+var2;
+    } 
+        
+    function get( 
+    ) public view returns (uint) { 
+        return sum; 
+    } 
+}
